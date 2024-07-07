aa81d20b7e39f57cfefb01f2691aa89ec95624a6
Pasan I Samarathunga <pasan.isam.office1@gmail.com>
2024-07-07 09:53:18 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// https://restcountries.com/v3.1/all
+
+
+let tblCountries = document.getElementById("tblCountries");
+
+let tableBody = `      <tr>
+            <th>Name</th>
+            <th>Flag</th>
+        </tr>` ;
+
+
+
+fetch("https://restcountries.com/v3.1/all")
+.then((res)=>res.json())
+.then(data=>{
+
+    data.forEach(element => {
+
+        tableBody+=`<tr>
+                        <td>${element.name.common}</td>
+                        <td><img src="${element.flags.png}"></td>
+                    </tr>`
+        console.log(element.name.common);
+    });
+
+    tblCountries.innerHTML=tableBody;
+})
+
+
