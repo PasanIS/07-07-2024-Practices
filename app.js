c687603d90d0a19b8380ff2bbad6f82fbc16f538
Pasan I Samarathunga <pasan.isam.office1@gmail.com>
2024-07-07 13:32:05 +0530

    Second Commit

--- app.js
+++ app.js
@@ -3,6 +3,20 @@
 
 let tblCountries = document.getElementById("tblCountries");
 
+function searchCountries() {
+    const searchInput = document.getElementById('countrySearch').value.toLowerCase();
+    const rows = document.querySelectorAll('#tblCountries tr');
+
+    rows.forEach((row) => {
+        const countryName = row.querySelector('td h1').textContent.toLowerCase();
+        if (countryName.includes(searchInput)) {
+            row.style.display = 'table-row';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+}
+
 let tableBody = `      <tr>
             <th>Name</th>
             <th>Flag</th>
@@ -11,19 +25,26 @@ let tableBody = `      <tr>
 
 
 fetch("https://restcountries.com/v3.1/all")
-.then((res)=>res.json())
-.then(data=>{
+    .then((res) => res.json())
+    .then(data => {
 
-    data.forEach(element => {
+        data.forEach(element => {
 
-        tableBody+=`<tr>
-                        <td>${element.name.common}</td>
+            tableBody += `<tr>
+                        <td><h1>${element.name.common}</h1> <br>
+
+                        Official Name : ${element.name.official}
+                        <p>Region : ${element.region} </p>
+                        <p>Population : ${element.population} </p>
+                        <a class="btn btn-primary" href="${element.maps.googleMaps}">Go To Map</a>
+                        
+                        </td>
                         <td><img src="${element.flags.png}"></td>
                     </tr>`
-        console.log(element.name.common);
-    });
+            console.log(element.name.common);
+        });
 
-    tblCountries.innerHTML=tableBody;
-})
+        tblCountries.innerHTML = tableBody;
+    })
 
 
